0d3dd7099b6dea7b1e874b3cc66b9e99fc9c9e83
Zachary Schultz <zachary.schultz@ibm.com>
2017-08-14 16:30:39 -0500

    adding in dynamic file extensions as well

--- blueprint/manifest_replace.sh
+++ blueprint/manifest_replace.sh
@@ -6,12 +6,25 @@
 # https://github.com/docker/compose/issues/495#issuecomment-126551019
 
 # Vars
-run_time_token="{{ run_time }}" # find this token
-run_time=$RUNTIME
+runtime_token="{{ runtime }}" # find this token
+runtime=$RUNTIME
+runtime_extension_token="{{ runtime_extension }}"
+case "${runtime}" in
+  "python") runtime_extension="py"
+  ;;
+  "swift") runtime_extension="swift"
+  ;;
+  "php") runtime_extension="php"
+  ;;
+  "node") runtime_extension="js"
+  ;;
+esac
 
-echo ${run_time_token} = ${run_time}
+echo ${runtime_token} = ${runtime}
+echo ${runtime_extension_token} = ${runtime_extension}
 
 # Find and replace
-sed -e "s/${run_time_token}/${run_time}/g" \
+sed -e "s/${runtime_token}/${runtime}/g" \
+    -e "s/${runtime_extension_token}/${runtime_extension}/g" \
     < manifest-template.yaml \
     > manifest.yaml
